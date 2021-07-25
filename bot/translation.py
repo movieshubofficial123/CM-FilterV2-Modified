760e330f3ca04f83786a4980c62c3dc1334c62c7
Call me...leo <74614943+its-leo-bitch@users.noreply.github.com>
2021-07-25 10:46:45 +0530

    #Edit 🤣
    Hmm

---
+++ bot/translation.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+class Translation(object):
+    
+    START_TEXT = """ <b>Hi {} 💃
+Am Just A Advance Auto Filter Bot....😉\n
+
+Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝\n
+
+For More Details Click /help Button 🥵.. @Leos_Tg\n
+#StopPiracy #GoLegal</b>"""
+
+    
+    HELP_TEXT = """
+<b><i><u>How To Use Me!?</u></i></b>
+
+*READ THIS* 👉 <i><a href="https://telegra.ph/Version-29-07-24">Itz All About Version 2.9</a></i>
+
+"""
+
+  
+
+   
+           
+
+            
+    
+            
+
+
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <code>Version 2.9</code>
+    
+➥ 𝕾𝖔𝖚𝖗𝖈𝖊 𝕮𝖔𝖉𝖊: <b><i><a href="https://t.me/Leos_TG">Leos_Tg</a></i></b>
+
+<b>➥ Language</b> : <code>Malayalam</code>
+
+<b>➥ 𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 𝐎𝐰𝐧𝐞𝐝 𝐁𝐲</b> : Leo
+"""
+
+
+
+
